936d5a4b1d98b0a485c6961b8f80a5c9de30a018
levannghia <levannghia.tdc2018@gmail.com>
2024-12-26 15:30:27 +0700

    Len

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GroupRequest;
 use App\Models\ChatGroup;
+use App\Models\ChatMessage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,9 +30,23 @@ class GroupController extends Controller
                 'description' => $request->description,
             ]);
 
+            $groupMembers = collect([auth()->id(), ...$request->group_members])
+                            ->map(fn ($member_id) => compact('member_id'));
+            $group->group_members()->createMany($groupMembers);
+            $chat = ChatMessage::create([
+                'from_id' => auth()->id(),
+                'body' => 'Create Group ' . $group->name,
+                'to_id' => $group->id,
+                'to_type' => ChatGroup::class,
+            ]);
+
             DB::commit();
+            return to_route('chats.show', $group->id);
         } catch (\Exception $e) {
             DB::rollBack();
+            return back()->with([
+                'error_msg' => $e->getMessage(),
+            ]);
         }
     }
 }
