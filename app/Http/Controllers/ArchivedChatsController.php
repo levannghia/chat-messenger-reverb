52de8da001f334273fe69d18a65478004892a797
levannghia <levannghia.tdc2018@gmail.com>
2025-01-15 14:41:56 +0700

    Len

--- app/Http/Controllers/ArchivedChatsController.php
+++ app/Http/Controllers/ArchivedChatsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChatGroup;
+use App\Models\ChatMessage;
+use App\Models\User;
 use App\Traits\Chat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Request as FacadesRequest;
@@ -16,8 +19,8 @@ class ArchivedChatsController extends Controller
         try {
             FacadesRequest::merge(['archived_chats' => true]);
 
-            return Inertia::render('archivedChats/Index', [
-                'chats' => fn () => $this->chats()
+            return Inertia::render('ArchivedChats/Index', [
+                'chats' => fn() => $this->chats()
             ]);
         } catch (\Exception $e) {
             return back()->with([
@@ -28,6 +31,41 @@ class ArchivedChatsController extends Controller
 
     public function show(string $id)
     {
-        
+        try {
+            FacadesRequest::merge(['archived_chats' => true]);
+            $user = User::find($id);
+            $group = ChatGroup::find($id);
+
+            if (!$user && !$group) {
+                throw new \Exception("User or group not found");
+            }
+
+            if ($user) {
+                $user->is_contact_saved = auth()->user()->is_contact_saved($id);
+                $user->is_contact_blocked = auth()->user()->is_contact_blocked($id);
+                $user->chat_type = ChatMessage::CHAT_TYPE;
+            } else if($group) {
+                $user = $group;
+                $user->creator = $group->creator;
+                $user->chat_type = ChatMessage::CHAT_GROUP_TYPE;
+                $user->members_count = $group->group_members->count();
+            }
+
+            $user->message_color = auth()->user()->message_color($id);
+
+            return Inertia::render('ArchivedChats/Show', [
+                'user' => fn () => $user,
+                'chats' => fn () => $this->chats(),
+                'messages' => fn () => $this->messages($id),
+                'media' => fn () => $this->media($id),
+                'files' => fn () => $this->files($id),
+                'links' => fn () => $this->links($id),
+            ]);
+
+        } catch (\Exception $e) {
+            return back()->with([
+                'error_msg' => $e->getMessage()
+            ]);
+        }
     }
 }
--- resources/js/Pages/ArchivedChats/Show.jsx
+++ resources/js/Pages/ArchivedChats/Show.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { ChatProvider } from "@/store/useChatStore";
+import { ChatMessageProvider } from "@/store/chatMessageStore";
+import { ModalProvider } from "@/contexts/modal-context";
 
-export default function Show() {
+import AppLayout from "@/layouts/AppLayout";
+import SidebarMini from "@/layouts/partials/SidebarMini";
+import Sidebar from "@/components/archivedChats/Sidebar";
+import Content from "@/components/chats/Content";
+import SidebarRight from "@/components/chats/SidebarRight";
+import PopupGallery from "@/components/chats/PopupGallery";
+
+export default function Chats() {
   return (
-    <div>Show</div>
-  )
+    <AppLayout title="Archived Chats">
+      <ChatProvider>
+        <ChatMessageProvider>
+          <ModalProvider>
+            <SidebarMini />
+            <Sidebar />
+            <Content />
+            <SidebarRight />
+            <PopupGallery />
+          </ModalProvider>
+        </ChatMessageProvider>
+      </ChatProvider>
+    </AppLayout>
+  );
 }
